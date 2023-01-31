149dab551fdd59dd566c0b8575738ddf425617b5
darkvader <paulo.deen@gmail.com>
2023-01-31 04:14:54 +0100

    Change text on landingspage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <div class="flex flex-col justify-center items-center">
   <div>
     <img src="https://firebasestorage.googleapis.com/v0/b/svelte-starter-8ef85.appspot.com/o/images%2Fmasaaki-komori-_SbeCWYjwCQ-unsplash.jpg?alt=media&token=4ab2cabb-f939-41a2-9f12-7af3a1e8b253" 
-      class="mb-8 mx-auto h-96 w-[48rem] object-cover"
+      class="mb-8 mx-auto h-80 w-[48rem] object-cover"
       alt="flower blossom">
   </div>
   <div class="mb-8 w-64 md:w-72">
-    <h1>Het genot van ontspannings massage</h1>
-    <p>YoYo Beauty & Massage</p>
+    <h1>Genieten van ontspannings massage</h1>
+    <p>YoYo Beauty & Massage salon</p>
     <p>'s-Gravendijkwal 145B</p>
     <p>3021 EL Rotterdam</p>
     <h2>Openingstijden:</h2>
