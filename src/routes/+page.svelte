0097268203ba3e50b8b66cd043a768bb0080cfa8
darkvader <paulo.deen@gmail.com>
2023-01-31 02:38:47 +0100

    Create initial landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center">
+  <div>
+    <img src="https://firebasestorage.googleapis.com/v0/b/svelte-starter-8ef85.appspot.com/o/images%2Fmasaaki-komori-_SbeCWYjwCQ-unsplash.jpg?alt=media&token=4ab2cabb-f939-41a2-9f12-7af3a1e8b253" 
+      class="mb-8 mx-auto h-96 w-[48rem] object-cover"
+      alt="flower blossom">
+  </div>
+  <div class="mb-8 w-64 md:w-72">
+    <h1>Het genot van ontspannings massage</h1>
+    <p>YoYo Beauty & Massage</p>
+    <p>'s-Gravendijkwal 145B</p>
+    <p>3021 EL Rotterdam</p>
+    <h2>Openingstijden:</h2>
+    <p>Maandag - zaterdag: 10:00 - 22:00</p>
+    <p>Zondag: 12:00 - 20:00</p>
+    <h2>Prijslijst:</h2>
+    <p>30 minuten vanaf 25 euro</p>
+    <p>60 minuten vanaf 40 euro</p>
+  </div>
+</div>
