9111b4485d8240f3794443ea04bfd0fbd3c651d3
Paulo Deen <paulo.deen@gmail.com>
2023-02-10 23:56:49 +0100

    Move text to match photos Make display for photos wider

--- src/routes/massages/+page.svelte
+++ src/routes/massages/+page.svelte
@@ -8,26 +8,28 @@
 		het lichaamsherstel en verbetert de slaapkwaliteit door het verminderen van spanning en het
 		verbeteren van de mentale ontspanning.
 	</p>
-	<h2>Sportmassage</h2>
-	<p>
-		Sportmassage is gericht op het verbeteren van de prestaties en het herstel van spieren en
-		gewrichten na fysieke inspanning. Het verhoogt de flexibiliteit en de beweeglijkheid van spieren
-		en gewrichten en vermindert spierspanning en pijn. Sportmassage bevordert de bloedcirculatie en
-		het transport van afvalstoffen uit de spieren, waardoor spierherstel wordt versneld.
-	</p>
-	<div class="relative mt-5">
-		<img
-			src="https://firebasestorage.googleapis.com/v0/b/svelte-starter-8ef85.appspot.com/o/images%2Fpexels-anete-lusina-5240677.jpg?alt=media&token=5370f1d3-367e-4777-be67-3a638d6c24fb"
-			class="mx-auto mb-5 h-52 w-[48rem] object-cover md:h-72"
-			alt="foot massage"
-		/>
-		<div class="absolute top-0 h-full w-full bg-rose-100 opacity-50" />
-	</div>
 	<h2>Voetmassage</h2>
 	<p>
 		Voetmassage heeft een positief effect op het hele lichaam. Door de druk die wordt uitgeoefend op
 		de voeten en enkels wordt de bloedcirculatie verbeterd en de zuurstof- en voedingsstoffen
 		toevoer verhoogd. Dit leidt tot vermindering van pijn en stijfheid in de voeten en enkels en een
 		verbetering van het lichaamsbalans.
+	</p>
+</div>
+<div class="relative mx-3 mt-5 max-w-3xl">
+	<img
+		src="https://firebasestorage.googleapis.com/v0/b/svelte-starter-8ef85.appspot.com/o/images%2Fpexels-anete-lusina-5240677.jpg?alt=media&token=5370f1d3-367e-4777-be67-3a638d6c24fb"
+		class="mx-auto mb-5 h-52 w-[48rem] object-cover md:h-72"
+		alt="foot massage"
+	/>
+	<div class="absolute top-0 h-52 w-full bg-rose-100 opacity-50" />
+</div>
+<div class="mx-3 max-w-3xl">
+	<h2 class="mt-2">Sportmassage</h2>
+	<p>
+		Sportmassage is gericht op het verbeteren van de prestaties en het herstel van spieren en
+		gewrichten na fysieke inspanning. Het verhoogt de flexibiliteit en de beweeglijkheid van spieren
+		en gewrichten en vermindert spierspanning en pijn. Sportmassage bevordert de bloedcirculatie en
+		het transport van afvalstoffen uit de spieren, waardoor spierherstel wordt versneld.
 	</p>
 </div>
