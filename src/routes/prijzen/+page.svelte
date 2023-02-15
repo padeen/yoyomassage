fb1fded3dde6ea3204497c39ee196223b15a52fb
Paulo Deen <paulo.deen@gmail.com>
2023-02-15 12:28:57 +0100

    Add full pricelist

--- src/routes/prijzen/+page.svelte
+++ src/routes/prijzen/+page.svelte
@@ -1,6 +1,64 @@
-<div class="mx-5 max-w-3xl">
+<div class="mx-5 max-w-xs">
 	<h1>Top kwaliteit zachte prijzen</h1>
-	<h2>Prijslijst:</h2>
-	<p>30 minuten vanaf 25 euro</p>
-	<p>60 minuten vanaf 40 euro</p>
+	<h2>Ontspanningsmassage</h2>
+	<table class="table-auto">
+		<tbody>
+			<tr>
+				<td class="pr-5">30 minuten</td>
+				<td>€25</td>
+			</tr>
+			<tr>
+				<td>60 minuten</td>
+				<td>€40</td>
+			</tr>
+			<tr>
+				<td>90 minuten</td>
+				<td>€60</td>
+			</tr>
+		</tbody>
+	</table>
+	<h2>Sportmassage</h2>
+	<table class="table-auto">
+		<tbody>
+			<tr>
+				<td class="pr-5">30 minuten</td>
+				<td>€30</td>
+			</tr>
+			<tr>
+				<td>60 minuten</td>
+				<td>€45</td>
+			</tr>
+			<tr>
+				<td>90 minuten</td>
+				<td>€65</td>
+			</tr>
+		</tbody>
+	</table>
+	<h2>Voetmassage</h2>
+	<table class="table-fixed">
+		<tbody>
+			<tr>
+				<td class="pr-5">30 minuten</td>
+				<td>€30</td>
+			</tr>
+			<tr>
+				<td>60 minuten</td>
+				<td>€45</td>
+			</tr>
+			<tr>
+				<td>90 minuten</td>
+				<td>€65</td>
+			</tr>
+		</tbody>
+	</table>
+	<div class="ml-1 mt-7 mb-9 w-16 border border-pink-200" />
+	<h2 class="mb-0">Pedicure</h2>
+	<p class="ml-0.5">€30</p>
+	<h2 class="mb-0 mt-2">Voetenbad Chinese kruiden</h2>
+	<p class="ml-0.5">€30</p>
+	<div class="ml-1 mt-7 mb-9 w-16 border border-pink-200" />
+	<h2 class="mb-0">Gezicht reinigen + gezichtsmasker</h2>
+	<p class="ml-0.5">€39</p>
+	<h2 class="mb-0 mt-2">Douchen</h2>
+	<p class="ml-0.5">€5</p>
 </div>
