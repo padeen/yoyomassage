0f7028266e5f8ef909faece509bfec4d63d3b672
Paulo Deen <paulo.deen@gmail.com>
2023-02-16 17:35:08 +0100

    Add bereikbaarheid and maps to contact page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,9 +16,10 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Montserrat&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Montserrat:wght@400;600&display=swap"
 		rel="stylesheet"
 	/>
+	<title>YoYo Beauty & Massage</title>
 </svelte:head>
 
 <div class="mb-5 flex flex-col items-center justify-center">
