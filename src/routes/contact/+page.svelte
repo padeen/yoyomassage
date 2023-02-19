d77e8caf865d1d8fd6f9fb0dad167533c42ffd56
Paulo Deen <paulo.deen@gmail.com>
2023-02-19 14:21:40 +0100

    Add callable link to contact and footer

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,14 @@
 <div class="w-full max-w-3xl">
 	<div class="mx-auto max-w-xs">
-		<h2>Bereikbaarheid</h2>
+		<h1>Vragen of afspraak maken</h1>
+		<a href="tel:0687109280">
+			<div class="text-center">
+				<img class="mx-auto mb-5 mt-5 h-10" src="/images/phone-call-pink.svg" alt="bellen" />
+				<span class="ml-0.5 text-2xl font-bold text-pink-400">06-87109280</span>
+			</div>
+		</a>
+		<div class="mx-auto mt-8 mb-9 w-16 border border-gray-300 md:hidden" />
+		<h2 class="mt-8">Bereikbaarheid</h2>
 		<p class="font-semibold text-slate-800">Tramlijn 4 halte 's-Gravendijkwal</p>
 		<p class="mb-3">Loopafstand 2 minuten</p>
 		<p class="font-semibold text-slate-800">Metrostation Dijkzigt</p>
